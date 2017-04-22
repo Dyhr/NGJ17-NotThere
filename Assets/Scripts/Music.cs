ac174af2dadb362c1b58e1f063c958420f234a23
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2017-04-22 23:07:18 +0200

    Basic music system

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+    public AudioSource safeSource, tenseSource, chaseSource;
+
+    public int level;
+    public float lerpSpeed = 0.05f;
+    public float volume = 0.15f;
+    private float intensity;
+
+	private void Update () {
+	    intensity = Mathf.Lerp(intensity, level, lerpSpeed);
+
+	    safeSource.volume = Mathf.Clamp(1 - Mathf.Abs(0 - intensity), 0, volume);
+	    tenseSource.volume = Mathf.Clamp(1 - Mathf.Abs(1 - intensity), 0, volume);
+	    chaseSource.volume = Mathf.Clamp(1 - Mathf.Abs(2 - intensity), 0, volume);
+	}
+}
